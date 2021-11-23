04a7c311b2bf2ec83c14934f4abf65b18cbe9ae9
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2021-11-23 21:27:37 +0100

    added audio element and autoplay to TimerControl component

--- src/components/TimerControl.tsx
+++ src/components/TimerControl.tsx
@@ -12,7 +12,7 @@ type TimerData = {
 
 type TimerProps = {
   data:TimerData
-}
+};
 
 
 export const TimerControl = (props:TimerProps) => {
@@ -29,21 +29,20 @@ export const TimerControl = (props:TimerProps) => {
     sessionTimeLeft.current = sessionTime;
     breakTimeLeft.current = breakTime;
     setDisplayedTime(formatTime(sessionTime));
-    // return () => {
-    //   cleanup
-    // }
-  }, [sessionTime, breakTime])
+  }, [sessionTime, breakTime]);
 
 
-  function startTimer () {
+  function startTimer (): void {
     if (timerIsRunning && timer !== null) {
 
       timerStopped();
+
       clearInterval(timer);
 
     } else if (!timerIsRunning) {
 
       timerStarted();
+      
       setTimer ( setInterval(()=> {
 
         //start with sessiontime reset if both session and break time is expired
@@ -52,29 +51,30 @@ export const TimerControl = (props:TimerProps) => {
           breakTimeLeft.current = breakTime;
           setDisplayedTime(formatTime(sessionTimeLeft.current));
           setTimerLabel("Session");
+          playAudio();
         } else
         //when session time is expired start break time
         if(sessionTimeLeft.current === 0) {
+          audioElement.current?.play();
           setDisplayedTime(formatTime(breakTimeLeft.current));
           breakTimeLeft.current = breakTimeLeft.current - ONE_SECOND;
           setTimerLabel("Break");
-          console.log("breakTimeLeft: " + breakTimeLeft.current)
+          playAudio();
         } else
         
         //start with sessiontime
         if (sessionTimeLeft.current > 0) {
           sessionTimeLeft.current = sessionTimeLeft.current - ONE_SECOND;
           setDisplayedTime(formatTime(sessionTimeLeft.current));
-          console.log("sessionTimeLeft: " + sessionTimeLeft.current)
-        }
-        
+        };
         
       }, 1000));
 
-    } 
+    }
   };
 
-  function resetTimer () {
+  
+  function resetTimer (): void {
     timerStopped();
     if (timer !== null) {
       clearInterval(timer);
@@ -83,8 +83,18 @@ export const TimerControl = (props:TimerProps) => {
     sessionTimeLeft.current = sessionTime;
     breakTimeLeft.current = breakTime;
     setDisplayedTime(formatTime(sessionTimeLeft.current));
-    setTimerLabel("Session")
-
+    setTimerLabel("Session");
+    stopAudio();
+  };
+  
+  const audioElement = useRef<HTMLAudioElement>(null);
+  function playAudio () {
+    audioElement.current?.play();
+  };
+  
+  function stopAudio () {
+    audioElement.current?.pause();
+    audioElement.current!.currentTime = 0;
   };
 
 
@@ -94,6 +104,7 @@ export const TimerControl = (props:TimerProps) => {
       <span id="time-left">{displayedTime}</span>
       <button id="start_stop" onClick={()=> startTimer()}>start_stop</button>
       <button id="reset" onClick={()=> resetTimer()}>reset</button>
+      <audio id="beep" ref={audioElement} src="https://soundbible.com/mp3/Fire_pager-jason-1283464858.mp3" />
     </div>
   );
 
